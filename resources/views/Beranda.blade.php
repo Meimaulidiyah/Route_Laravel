4858b3f22d588611733685423fed54000fcf60ac
Meimaulidiyah <meimaulidiyah.04@gmail.com>
2021-08-13 00:48:36 +0700

    route_laravel

---
+++ resources/views/Beranda.blade.php
@@ -0,0 +1,171 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style type ="text/css">
+    .carousel-item {
+  height: 100vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+section {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+}
+
+.lnr {
+  display: inline-block;
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.05em;
+  stroke-width: 1;
+}
+
+.services-icon {
+  margin-bottom: 20px;
+  font-size: 30px;
+}
+bgc2, .vLine, .hLine {
+    background-color: #0F52BA;
+}
+
+.quote-icon {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+.services-icon {
+    font-size: 60px;
+    margin-left: 2rem;
+}
+
+    </style>
+    <title>Laravel</title>
+  </head>
+  <body>
+    <h1>Laravel</h1>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar- bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="beranda">Beranda
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profil.blade.php">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Gallery.blade.php">Gallery</a>
+        </li>       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<header>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('https://i.pinimg.com/originals/df/73/64/df736472c1e9f4cafc50963e7ccf47a5.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">First Slide</h2>
+          <p class="lead">This is a description for the first slidephp artisan serve.</p>
+        </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://i.pinimg.com/originals/6a/95/64/6a9564d182da0524afbc46e543cebc00.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Second Slide</h2>
+          <p class="lead">This is a description for the second slide.</p>
+        </div>
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://i.pinimg.com/originals/bf/b4/17/bfb41746e3f38f99ce3b3ddb8ab6f920.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Third Slide</h2>
+          <p class="lead">This is a description for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
+</header>
+
+<section class="main">
+<div class="container mt-4">
+    <h1 class="text-center mb-4 p-4 text-dark">PENGETAHUAN</h1>
+      <div class="row">
+
+   <div class="card-columns">
+  <div class="card shadow border-0">
+    <img class="card-img-top" src="https://i.pinimg.com/originals/b8/43/e7/b843e7f5418e728d2224152f14556d0a.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Laravel</h5>
+      <h5><a href="https://www.niagahoster.co.id/blog/laravel-adalah/">Pengertian Laravel</a></h5>      
+    </div>
+  </div>
+ 
+  <div class="card shadow border-0">
+    <img class="card-img-top" src="https://i.pinimg.com/originals/b4/8f/27/b48f27e9ad42566052ecd0fc471f2c34.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HTML</h5>
+      <h5><a href="https://www.niagahoster.co.id/blog/html-adalah/">Pengertian HTML</a></h5>          
+    </div>
+  </div>
+ 
+  <div class="card shadow border-0">
+    <img class="card-img" src="https://i.pinimg.com/originals/15/00/d0/1500d01a918e1df4e389926937f8887d.jpg" alt="Card image">
+  <div class="card shadow border-0">
+    <div class="card-body">
+      <h5 class="card-title">CSS</h5>
+      <h5><a href="https://www.niagahoster.co.id/blog/pengertian-css/">Pengertian CSS</a></h5>            
+    </div>
+  </div>
+</div>
+</div>
+   </div>
+   </section>
+  
+<!-- /.container -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
+
+  </body>
+</html>
---
+++ resources/views/Gallery.blade.php
@@ -0,0 +1,207 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style type ="text/css">
+    .carousel-item {
+  height: 100vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+section {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+}
+
+.lnr {
+  display: inline-block;
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.05em;
+  stroke-width: 1;
+}
+
+.services-icon {
+  margin-bottom: 20px;
+  font-size: 30px;
+}
+bgc2, .vLine, .hLine {
+    background-color: #0F52BA;
+}
+
+.box {
+  margin:10px;
+}
+
+.quote-icon {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+.services-icon {
+    font-size: 60px;
+    margin-left: 2rem;
+}
+
+    </style>
+    <title>Laravel</title>
+  </head>
+  <body>
+    <h1>Laravel</h1>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar- bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="beranda">Beranda
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profil.blade.php">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Gallery.blade.php">Gallery</a>
+        </li>       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Page Content -->
+<section class="py-5 text-center">
+      <div class="container"> 
+        <h2 class="text-center">From The Moment</h2>
+        <p class="text-muted mb-5 text-center">Sometimes you will never know the value of a moment until becomes a memory.</p>
+        <div class="row">
+          
+          <div class="card shadow border-5">
+            <img class="card-img-top" src="https://i.pinimg.com/originals/d1/1d/ae/d11dae82823850faf1ca61a2a12cc57d.jpg" alt="Card image cap"  height="650px" width="350px">
+            <div class="card-body">
+              <h5 class="card-title">SMKN 1 Purwosari Class 12 RPL 2</h5>
+              <p class="card-text">You are the creator for you own future.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+
+            <tr>
+              <div class="box">
+              <td><img src="https://i.pinimg.com/originals/be/7d/6b/be7d6bb36348f46390259a874645f42c.jpg" width="270px" height="200px"> </td>
+              <td><img src="https://i.pinimg.com/originals/ac/07/48/ac07484d88fcd136c4e3dab831ca1506.jpg" width="135px" height="250px"> </td>
+              <td><img src="https://i.pinimg.com/originals/06/21/ef/0621ef2355f03ac79b6ad4391a3728cb.jpg" width="150px" height="250px"> </td>
+              <td><img src="https://i.pinimg.com/originals/e0/50/67/e05067214f7becbdb4bf6ab596901b44.jpg"  width="135px" height="250px"></td>
+              <td><img src="https://i.pinimg.com/originals/c8/da/85/c8da8516833f68ad67dd0d1212b3827f.jpg"  width="270px" height="230px"></td>  
+              </tr>      
+              <tr>
+                <div class="box">
+                <td><img src="https://i.pinimg.com/originals/1d/80/6a/1d806a533778b68c6acb6a3bb08ab457.jpg" width="270px" height="200px"> </td>
+                <td><img src="https://i.pinimg.com/originals/7a/d5/bf/7ad5bfb00a1dcff08c18c14fefdd81a2.jpg" width="270px" height="200px"> </td>                            
+                </tr>      
+
+              
+              <div class="box">
+                <img class="card-img-top" src="https://i.pinimg.com/originals/98/52/4c/98524c466fa59ec7fad857864e238a5b.jpg" alt="Card image cap"  height="650px" width="400px">
+                <div class="card-body">
+                  <h5 class="card-title">SMKN 1 Purwosari Scout</h5>
+                  <p class="card-text">You are the creator for you own future.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+                
+              <tr>
+              <div>
+              <td><img src="https://i.pinimg.com/originals/6d/7d/a3/6d7da31496448da238f3c7b76aa3e1fd.jpg" width="250px" height="200px"> </td>
+              <td><img src="https://i.pinimg.com/originals/2b/18/73/2b18736954c65f83f8b608d9d6952abc.jpg"  width="135px" height="250px"> </td>
+              <td><img src="https://i.pinimg.com/originals/2b/bd/f6/2bbdf616400c795739d1fc20d4ef38f2.jpg"  width="230px" height="200px"> </td>                           
+              </tr>
+              <tr>
+                <div class="box">
+                <td><img src="https://i.pinimg.com/originals/62/71/d7/6271d74ce6112bbab2db39f3c576e341.jpg" width="300px" height="200px"></td>
+                <td><img src="https://i.pinimg.com/originals/79/1d/d8/791dd81ad4ac1ed9ae82c52aa4f016fe.jpg"  width="270px" height="200px"></td> 
+                </tr>
+              
+              <div class="box">
+                <img class="card-img-top" src="https://i.pinimg.com/originals/f3/f6/26/f3f626fb7642408e8d298ccd27981f7c.jpg" alt="Card image cap"  height="500px" width="400px">
+                <div class="card-body">
+                  <h5 class="card-title">SMKN 1 Purwosari Prakerin</h5>
+                  <p class="card-text">You are the creator for you own future.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+
+                <tr>
+                  <div>
+                  <td><img src="https://i.pinimg.com/originals/35/65/67/35656768db518ea1e90a61fa421ac9db.jpg" width="150px" height="250px"> </td>
+                  <td><img src="https://i.pinimg.com/originals/ae/bc/b1/aebcb134f2c13feff4d7640266265396.jpg"  width="250px" height="200px"> </td>
+                  <td><img src="https://i.pinimg.com/originals/21/75/89/217589541153ca1f455a38a6a5b7ac08.jpg"  width="250px" height="200px"> </td>
+                  <td><img src="https://i.pinimg.com/originals/7d/c7/74/7dc77440f0d80ae7c2d81396649d122e.jpg"  width="250px" height="200px"></td>
+                </tr>
+                
+                <tr>
+                 <div class="box">
+                <td><img src="https://i.pinimg.com/originals/93/cf/9c/93cf9ca1a12bfe8ed92594a96f168372.jpg"  width="250px" height="200px"></td> 
+                <td><img src="https://i.pinimg.com/originals/43/36/df/4336df829110641ba808221465535659.jpg"  width="280px" height="200px"> </td>
+                <td><img src="https://i.pinimg.com/originals/37/35/3a/37353af0202b1cf38d28431b8266c2f7.jpg"  width="260px" height="200px"></td>
+                </tr>
+                  <tr>
+                    <div class="box">
+                    <td><img src="https://i.pinimg.com/originals/61/0c/34/610c341f8acec4e9e84c3e7e34bd5941.jpg" width="120px" height="250px"> </td>
+                    <td><img src="https://i.pinimg.com/originals/b6/43/34/b6433426cc43a230ed220e60c73d3570.jpg"  width="250px" height="200px"> </td>                    
+                    <td><img src="https://i.pinimg.com/originals/db/70/2a/db702a5d55020488b7ee9c195804c9a5.jpg"  width="250px" height="200px"></td> 
+                    </tr>  
+                  
+                  <div class="box">
+                    <img class="card-img-top" src="https://i.pinimg.com/originals/52/56/68/525668f892cfabbc3b80d467a893df7e.jpg" alt="Card image cap"  height="500px" width="400px">
+                    <div class="card-body">
+                      <h5 class="card-title">Di Rumah Saja</h5>
+                      <p class="card-text">You are the creator for you own future.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+    
+                    <tr>
+                      <div>
+                      <td><img src="https://i.pinimg.com/originals/60/6c/0a/606c0a592382e9d764609eeff9f0e45f.jpg" width="135px" height="250px"> </td>
+                      <td><img src="https://i.pinimg.com/originals/61/fc/90/61fc9019776084623836757fcf238c81.jpg"  width="135px" height="250px"> </td>
+                      <td><img src="https://i.pinimg.com/originals/13/c3/6f/13c36f73e3b1e6ccd2a9e9f07bea5d01.jpg"  width="135px" height="250px"> </td>
+                      <td><img src="https://i.pinimg.com/originals/8f/25/35/8f2535bd8314a3e1733f3897c93ade02.jpg"  width="135px" height="250px"></td>
+                      <td><img src="https://i.pinimg.com/originals/1c/dc/f8/1cdcf8e247b21374d05b69745af5ed86.jpg"  width="135px" height="250px"></td> 
+                      </tr>  
+                      <tr>
+                        <div class="box">
+                        <td><img src="https://i.pinimg.com/originals/03/a2/82/03a282828d895aaa3801747b2ca03318.jpg" width="135px" height="250px"> </td>
+                        <td><img src="https://i.pinimg.com/originals/ea/9d/77/ea9d772d6fc9e1887ea6447966268017.jpg"  width="135px" height="250px"> </td>
+                        <td><img src="https://i.pinimg.com/originals/b4/47/24/b447244ff126ddbb49af9ee0e434310b.jpg"  width="135px" height="250px"> </td>
+                        <td><img src="https://i.pinimg.com/originals/d0/bc/42/d0bc421ae3bab5b2fcc5d80fbd915aaf.jpg"  width="135px" height="250px"></td>
+                        <td><img src="https://i.pinimg.com/originals/4b/63/f5/4b63f555ea7a82bb7a90c23ae39cb2ea.jpg"  width="135px" height="250px"></td> 
+                        </tr>  
+          
+        </div>
+      </div>
+</section>
+  
+<!-- /.container -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
+
+  </body>
+</html>
